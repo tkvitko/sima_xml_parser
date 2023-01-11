f60d20316a376d233ab153a1413d769e956a6e5c
Taras Kvitko <tkvitko@gmail.com>
2023-01-11 15:27:32 +0300

    enable download

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     url = read_url_from_file(os.path.join(WORK_DIR, SOURCE_FILENAME))
 
     if url:
-        # download_xml(url=url, dir=WORK_DIR)
+        download_xml(url=url, dir=WORK_DIR)
         convert_xml(dir=WORK_DIR)
         res = parse_filtered(os.path.join(WORK_DIR, FILTERED_FILENAME))
 
