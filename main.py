69c0006902391cfa9c99dab5d5d75da89f83dc60
Taras Kvitko <tkvitko@gmail.com>
2023-01-20 22:06:40 +0300

    uodate in db only if price has been changed or new product

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import configparser
 
 from parser.constants import WORK_DIR
 from parser.excel import create_xlsx
-from parser.mongo_cache import all_products_from_cache, add_products_to_cache
+from parser.mongo_cache import all_products_from_cache, add_products_to_cache, add_product_to_cache
 from parser.parser import parse_filtered
 from parser.sima_api import get_sima_bearer, get_product_by_article, get_sima_object_name_by_id
 from parser.tools import *
@@ -10,10 +10,10 @@ from parser.tools import *
 
 def get_product_data_from_sima(product_id, bearer):
     product_data = get_product_by_article(product_id, bearer)
-    product_data['country_id'] = get_sima_object_name_by_id(product_data['country_id'], 'country',
-                                                            bearer)
-    product_data['trademark_id'] = get_sima_object_name_by_id(product_data['trademark_id'], 'trademark',
-                                                              bearer)
+    product_data['country_id'] = get_sima_object_name_by_id(product_data['country_id'],
+                                                            'country', bearer)
+    product_data['trademark_id'] = get_sima_object_name_by_id(product_data['trademark_id'],
+                                                              'trademark', bearer)
     return product_data
 
 
@@ -25,13 +25,23 @@ def check_if_price_changed_enough(product):
     new_price = product['price']
     try:
         old_price = old_products[product['_id']]
+
+        # если нашли пробукт в кеше
+        if new_price != old_price:
+            # если цена отличается, обновляем
+            add_product_to_cache(product)
+
         if ((new_price / old_price) <= 1 * percent / 100) or ((new_price / old_price) >= 1 / (percent / 100)):
+            # если цена отличается достаточно сильно, добавляем на нотификацию
             logger.info(
                 f'Product {product["_id"]} (new price {new_price}, old price {old_price}) has been added to notification')
             return True
+
+        # если не достаточно сильно, не добавляем на нотификацию
         return False
     except KeyError:
-        # haven't find product by id in cache
+        # если продукт новый, добавляем в кеш, не добавляем на нотификацию
+        add_product_to_cache(product)
         return False
 
 
@@ -60,7 +70,6 @@ if __name__ == '__main__':
                 data.append(product_data)  # добавляем в данные для уведомления
                 logger.info(f'Product {product_id} has been added to excel')
 
-            add_products_to_cache(new_products)  # записываем в кеш новые данные по полученным продуктам
             logger.info(f'{len(new_products)} has been added/updated in cache')
             if data:
                 create_xlsx(data)  # создаем excel с достаточно изменившимися ценами
